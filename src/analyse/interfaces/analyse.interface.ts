6465764165ea91fc97705315998191a6b64feb29
liumin <970568830@qq.com>
2021-01-24 22:38:59 +0800

    analyse list

--- src/analyse/interfaces/analyse.interface.ts
+++ src/analyse/interfaces/analyse.interface.ts
@@ -1,3 +1,6 @@
 export interface Item {
   title: string;
+  createdAt?: string;
+  url?: string;
+  desc?: string;
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-export default axios.request;
+export default axios;
