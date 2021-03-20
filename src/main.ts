393cb49c0da083fc4ba352c76942aeb385912ef0
liumin <970568830@qq.com>
2021-03-21 02:38:09 +0800

    helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
