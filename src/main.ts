16d28b3e298007bb350376caa4b4d84954b74e85
liumin <970568830@qq.com>
2021-03-21 02:40:34 +0800

    启用跨域

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
   await app.listen(3000);
