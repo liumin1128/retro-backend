f0d60d2df02fc506f05792839e9ef8e7c53fc3cd
liumin <970568830@qq.com>
2021-03-21 02:57:48 +0800

    更新

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+
   app.use(helmet());
+  app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 1000, // limit each IP to 100 requests per windowMs
+    }),
+  );
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
