7a748b9d1f19e159d888edbe5fc9a7d3a779498b
liumin <970568830@qq.com>
2021-04-30 07:31:25 +0800

    feat：移除无用目录

--- src/cats/dto/create-cat.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class CreateCatDto {
-  @IsNotEmpty()
-  readonly name: string;
-  @IsNotEmpty()
-  readonly age: number;
-  readonly breed: string;
-}
