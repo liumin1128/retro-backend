c6700145697cfafc94865276f7f83965152943bf
liumin <min_liu@singaporeair.com.sg>
2021-11-29 01:55:19 +0800

    21

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(
     helmet({
       contentSecurityPolicy:
