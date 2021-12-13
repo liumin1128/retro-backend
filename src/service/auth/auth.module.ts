0b1c237e195c8ffa84264a51f9b4a52644cad389
liumin <min_liu@singaporeair.com.sg>
2021-12-14 07:45:45 +0800

    feat：延长token过期时间

--- src/service/auth/auth.module.ts
+++ src/service/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '6000s' },
+      signOptions: { expiresIn: '604800s' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
