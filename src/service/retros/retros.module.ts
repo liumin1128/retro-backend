3dbf017b37bb22fa940d36e63ab6c361b60df3c0
liumin <970568830@qq.com>
2022-02-03 18:53:58 +0800

    feat: 显示数据信息

--- src/service/retros/retros.module.ts
+++ src/service/retros/retros.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { RetrosService } from './retros.service';
-import { RetrosResolver } from './retros.resolver';
-import { Retro, RetroSchema } from './retros.schema';
-import { RetrosController } from './retros.controller';
+import { RetrosService } from '@/service/retros/retros.service';
+import { RetrosResolver } from '@/service/retros/retros.resolver';
+import { Retro, RetroSchema } from '@/service/retros/retros.schema';
+import { RetrosController } from '@/service/retros/retros.controller';
+import {
+  RetroMessage,
+  RetroMessageSchema,
+} from '@/service/retros/messages/schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Retro.name, schema: RetroSchema }]),
+    MongooseModule.forFeature([
+      { name: RetroMessage.name, schema: RetroMessageSchema },
+    ]),
   ],
   providers: [RetrosService, RetrosResolver],
   exports: [RetrosService],
--- src/service/retros/retros.service.ts
+++ src/service/retros/retros.service.ts
@@ -3,12 +3,16 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateRetroDto } from './retros.dto';
 import { Retro, RetroDocument } from './retros.schema';
+import { RetroMessage, RetroMessageDocument } from './messages/schema';
 
 @Injectable()
 export class RetrosService {
   constructor(
     @InjectModel(Retro.name)
     private readonly retrosModel: Model<RetroDocument>,
+
+    @InjectModel(RetroMessage.name)
+    private readonly retroMessagesModel: Model<RetroMessageDocument>,
   ) {}
 
   async create(createRetroDto: CreateRetroDto): Promise<RetroDocument> {
@@ -18,8 +22,68 @@ export class RetrosService {
     return createdRetro;
   }
 
-  async findAll(): Promise<RetroDocument[]> {
-    return this.retrosModel.find().populate('user');
+  async findAll(): Promise<any> {
+    // 不需要使用这种老套的方法
+    // happyCount: {
+    //   $sum: {
+    //     $cond: [{ $eq: ['$type', 'HAPPY'] }, 1, 0],
+    //   },
+    // },
+
+    // https://www.5axxw.com/questions/content/3l0r6i
+    const data = await this.retrosModel.aggregate([
+      // 关联查询retromessage信息
+
+      {
+        $lookup: {
+          from: 'retromessages',
+          let: { retro: '$_id' },
+          pipeline: [
+            { $match: { $expr: { $eq: ['$$retro', '$retro'] } } },
+            {
+              $group: {
+                _id: '$type',
+                count: { $sum: 1 },
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                k: '$_id',
+                v: '$count',
+              },
+            },
+          ],
+          as: 'count',
+        },
+      },
+
+      {
+        $addFields: {
+          count: { $arrayToObject: '$count' },
+        },
+      },
+
+      // 关联查询用户信息
+      {
+        $lookup: {
+          from: 'users',
+          let: { user: '$user' },
+          pipeline: [{ $match: { $expr: { $eq: ['$$user', '$_id'] } } }],
+          as: 'user',
+        },
+      },
+      {
+        $unwind: '$user',
+      },
+      {
+        $addFields: {
+          user: '$user',
+        },
+      },
+    ]);
+
+    return data;
   }
 
   async findById(_id: string): Promise<RetroDocument> {
