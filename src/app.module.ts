86f605a8815821f686a36d3f397d0fb905efdb37
liumin <970568830@qq.com>
2022-03-08 10:46:43 +0800

    fix: merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from '@/app.controller';
-<<<<<<< HEAD
-import { GraphqlModule } from '@/graphql/graphql.module';
-import services from '@/service/modules';
-=======
 import serviceModules from '@/service/modules';
->>>>>>> d195413 (fix: 更新文件结构)
 
 @Module({
   imports: [
@@ -40,13 +35,7 @@ import serviceModules from '@/service/modules';
       },
     }),
 
-<<<<<<< HEAD
-    GraphqlModule,
-
-    ...services,
-=======
     ...serviceModules,
->>>>>>> d195413 (fix: 更新文件结构)
   ],
   controllers: [AppController],
   providers: [],
